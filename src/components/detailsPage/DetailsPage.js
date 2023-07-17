8ff31a420e41f1a261d107e2e01a4f4cc9c997e9
kiscean <visocorodnii@gmail.com>
2023-07-17 13:09:24 +0300

    Work commit 17

--- src/components/detailsPage/DetailsPage.js
+++ src/components/detailsPage/DetailsPage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import {
-    Container,
+    Box, CardMedia,
+    Container, List, ListItemIcon, ListItemText,
     Typography
 } from "@mui/material";
 import Category from "../categoryList/Category";
+import guitars from '../../static/guitars.png';
+import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 
 const DetailsPage = () => {
     return (
@@ -31,6 +34,42 @@ const DetailsPage = () => {
             >
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Typography>
+            <Box
+               sx={{
+                   display:'flex',
+                   justifyContent: "center"
+               }}
+            >
+                <CardMedia
+                    sx={{
+                        height: "500px",
+                        width: "500px",
+                    }}
+                    component={"img"}
+                    image={guitars}
+                    alt={"guitars"}
+                />
+            </Box>
+            <Typography
+                variant={"body1"}
+                align={"center"}
+                m={2}
+            >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Typography>
+            <Typography
+                variant={"h3"}
+                align={"center"}
+                m={2}
+            >
+                Characteristics
+            </Typography>
+            <List>
+                <ListItemIcon>
+                    <DoubleArrowIcon />
+                    <ListItemText primary={"Neck"} />
+                </ListItemIcon>
+            </List>
         </Container>
     );
 };
