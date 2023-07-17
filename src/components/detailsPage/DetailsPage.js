e324df7a5edd6ebe4398878ed34564287ce0144f
kiscean <visocorodnii@gmail.com>
2023-07-17 16:12:57 +0300

    Work commit 17

--- src/components/detailsPage/DetailsPage.js
+++ src/components/detailsPage/DetailsPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {
     Box, CardMedia,
-    Container, List, ListItemIcon, ListItemText,
+    Container, List, ListItemButton, ListItemIcon, ListItemText,
     Typography
 } from "@mui/material";
 import Category from "../categoryList/Category";
@@ -58,17 +58,19 @@ const DetailsPage = () => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Typography>
             <Typography
-                variant={"h3"}
+                variant={"h6"}
                 align={"center"}
                 m={2}
             >
                 Characteristics
             </Typography>
             <List>
-                <ListItemIcon>
-                    <DoubleArrowIcon />
+                <ListItemButton>
+                    <ListItemIcon>
+                        <DoubleArrowIcon />
+                    </ListItemIcon>
                     <ListItemText primary={"Neck"} />
-                </ListItemIcon>
+                </ListItemButton>
             </List>
         </Container>
     );
